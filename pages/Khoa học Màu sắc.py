cb27a1d39e19619f62d60b056f56d30dfc57c87d
vyk07 <khanhvydldnc2908@gmail.com>
2025-05-09 14:02:25 +0700

    Add files via upload

---
+++ pages/Khoa học Màu sắc.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Khoa học Màu sắc", layout="wide"
+)
+st.write('## <p style="color:#ff8503">👀️🎨**Project: Khoa học Màu sắc️**🎨☁️ </span>'
+        '<p style="color:#018c67; font-size:30px"><span style="font-family: Roboto;">Dự án Khoa học về Màu và In ấn phục chế 2025', unsafe_allow_html=True )
+
+st.write('**Chào mừng bạn đến với <span style="color:#FF8C00"> Project: Khoa học Màu sắc </span> của mình!**', unsafe_allow_html=True)
+st.write('Mình tên là **Lê Ngọc Khánh Vy**, mã số sinh viên **22158110** - hiện là sinh viên năm 3 ngành **Công nghệ Kỹ thuật In**, thuộc khoa **In và Truyền thông** tại **Trường Đại học Sư phạm Kỹ thuật Thành phố Hồ Chí Minh**.')
+st.write('Đây là project môn học thứ ba mình được phụ trách. Nội dung của project lần này liên quan đến lĩnh vực khoa học màu sắc cung cấp cho các một góc nhìn mới lạ về màu, đồng thời cũng tiếp cận được một số khái niệm cơ bản về phục chế màu trong in ấn.')
+st.write("#### <p style='color:#018c67'> 👈**Let's get it!**", unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://static.vecteezy.com/system/resources/previews/008/058/831/non_2x/a-simple-modern-abstract-design-a-beautiful-soft-off-white-element-twisting-and-flowing-on-a-beige-background-illustration-vector.jpg"); 
+        background-size: cover; 
+    }
+    </style>""",
+    unsafe_allow_html=True
+)
